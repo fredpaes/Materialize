065bdb80e6f49139dbd4e594b10d7f850fef51f4
Fred Paucar <fred.paucarespinoza@gmail.com>
2019-02-03 19:17:41 -0500

    adding slider

--- js/script.js
+++ js/script.js
@@ -2,5 +2,12 @@
 ((d) => {
     d.addEventListener('DOMContentLoaded', e => {
         M.AutoInit()
+
+        const sliders = d.querySelectorAll('.slider')
+
+        M.Slider.init(sliders, {
+            indicators: true,
+            duration: 1000
+        })
     })
 })(document);
